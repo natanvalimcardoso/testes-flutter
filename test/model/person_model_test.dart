6622ac943dde8d4ae411fe5cd6956dc18938f9ad
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-04-18 08:57:54 -0300

    feat: continuidade

--- test/model/person_model_test.dart
+++ test/model/person_model_test.dart
@@ -2,11 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testes_masterclass/model/person_model.dart';
 
 void main() {
-  var person = PersonModel(height: 70, weight: 1.77);
   test(
     'Deve retornar 22.34 ao instanciar PersonModel com peso 70 e altura 1.77',
     () {
-      
+      var person = PersonModel(weight: 70, height: 1.77);
+      expect(person.getIMC(), 22.34351559258195);
     },
   );
 }
